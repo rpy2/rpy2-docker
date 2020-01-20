7653998b4e6e50b45210ecffa9fa96f3b2e65fac
Laurent Gautier <lgautier@gmail.com>
2020-01-20 13:23:18 -0500

    Add installation of Apache arrow for Python & R.
    ... if the Apache project has apt packages for the target ubuntu
    distribution.

---
+++ jupyter_ds/install_all_jupyter_ds.sh
@@ -0,0 +1,56 @@
+
+BUILDDEPS="libssl-dev"
+
+UBUNTU_RELEASE=$(lsb_release --release --short)
+
+# We rely on the apt packages from the Arrow project
+# but they are a little behind ubuntu releases.
+[ ! "${UBUNTU_RELEASE}" > "19.04" ]
+ARROW_AVAILABLE=$?
+
+apt-get update -qq
+
+if (( ARROW_AVAILABLE == 0 )); then
+
+  BUILDDEPS="${BUILDDEPS} \
+    libarrow-dev \
+    libarrow-glib-dev \
+    libarrow-flight-dev \
+    libplasma-dev \
+    libplasma-glib-dev \
+    libgandiva-dev \
+    libgandiva-glib-dev \
+    libparquet-dev \
+    libparquet-glib-dev"
+
+  wget -O /usr/share/keyrings/apache-arrow-keyring.gpg https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-keyring.gpg
+  tee /etc/apt/sources.list.d/apache-arrow.list <<APT_LINE
+deb [arch=amd64 signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
+deb-src [signed-by=/usr/share/keyrings/apache-arrow-keyring.gpg] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
+APT_LINE
+  apt-get update -qq
+
+fi
+
+apt-get install -y ${BUILDDEPS}
+
+python3 -m pip install --pre jupyter-lsp
+
+jupyter labextension install @krassowski/jupyterlab-lsp
+
+python3 -m pip install \
+         bash-language-server \
+	 python-language-server[all]
+
+R -e 'install.packages("languageserver")'
+
+if (( ARROW_AVAILABLE == 0 )); then
+    python3 -m pip install 'pyarrow==0.15.*'
+    R -e 'install.packages("arrow")'
+fi
+
+rm -rf /tmp/* 
+apt-get remove --purge -y ${BUILDDEPS}
+apt-get autoremove -y 
+apt-get autoclean -y
+rm -rf /var/lib/apt/lists/*
