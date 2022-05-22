92863b7491d69e4e03a6e167bbc5a246888e6444
Laurent Gautier <lgautier@gmail.com>
2022-05-22 15:33:42 -0400

    Fix rpy2 release, bump to R-4.

--- base/README.md
+++ base/README.md
@@ -8,19 +8,19 @@ docker build -t rpy2/base-ubuntu:master-default .
 This will try to build rpy2's master branch, but a specific
 branch/tag can be specified at build time.
 
-For example, to build an image with rpy2 release 3.4.6
+For example, to build an image with rpy2 release 3.5.2
 
 ```bash
-RPY2_VERSION=3.2.5
+RPY2_VERSION=3.5.2
 docker build -t rpy2/base-ubuntu:"${RPY2_VERSION}"-default --build-arg RPY2_VERSION="${RPY2_VERSION}" .
 ```
 
-The default ubuntu release is, at the time of writing, `20.04`
-but a different release can be specified at build time (e.g., `20.10`).
+The default ubuntu release is, at the time of writing, `22.04`
+but a different release can be specified at build time (e.g., `21.10`).
 
 For example:
 
 ```bash
-UBUNTU_RELEASE=20.04
+UBUNTU_RELEASE=22.04
 docker build -t rpy2/base-ubuntu:master-"${UBUNTU-RELEASE}" --build-arg UBUNTU_RELEASE=$UBUNTU_RELEASE .
 ```
--- base/install_apt.sh
+++ base/install_apt.sh
@@ -1,18 +1,14 @@
 apt-get update -qq
-apt-get install -y \
-        apt-utils \
-        apt-transport-https \
-	dirmngr \
-        gnupg \
-	libcurl4-openssl-dev \
-	libnlopt-dev \
-        lsb-release
-apt-key adv \
-        --keyserver keyserver.ubuntu.com \
-        --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+apt install -y --no-install-recommends \
+    software-properties-common \
+    dirmngr \
+    lsb-release
+    wget
+wget -qO- "${CRAN_MIRROR}"/bin/linux/ubuntu/marutter_pubkey.asc \
+    | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 add-apt-repository \
         --yes \
-        "deb {CRAN_MIRROR}/bin/linux/ubuntu/ $(lsb_release -c -s)${CRAN_MIRROR_TAG}/"
+        "deb ${CRAN_MIRROR}/bin/linux/ubuntu/ $(lsb_release -c -s)${CRAN_MIRROR_TAG}/"
 apt-get update -qq
 apt-get install -y \
         aptdaemon \
@@ -27,6 +23,5 @@ apt-get install -y \
 	python3-venv \
 	r-base \
 	r-base-dev \
-	sudo \
-	wget
+	sudo
 rm -rf /var/lib/apt/lists/*
